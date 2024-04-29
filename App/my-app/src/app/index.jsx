67733ca5d546e872d1969de61028c2dd5be6404d
MenossiJose <jantoniomenossi@gmail.com>
2024-04-29 14:10:02 -0300

    SignUp Screen

--- App/my-app/src/app/index.jsx
+++ App/my-app/src/app/index.jsx
@@ -1,20 +1,108 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { Link } from 'expo-router';
+import react from 'react';
+import {Image, StyleSheet, Pressable, Text,  TextInput,  View} from 'react-native';
+import { Link, router } from 'expo-router';
 
-export default function Home() {
+function Login() {
   return (
     <View style={styles.container}>
-        <Text>Home</Text>
-        <Link href={"/login"}>Login</Link>
-        </View>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={require('./img/Wolvesprojeto 1.png')} />
+      </View>
+      <View style={styles.containerFields}>
+        <TextInput style={styles.inputFields}
+          placeholder="Username"
+        />
+        <TextInput style={styles.inputFields}
+          placeholder="Password"
+        /> 
+      </View>
+      <View style={styles.buttonContainer}>
+        <Pressable style={styles.buttons} title="Login">
+          <Text>Login</Text>
+        </Pressable>
+        <Text style={styles.text}>Don't have an account?</Text>
+        <Pressable style={styles.buttons} title="Sign Up" onPress={() => router.replace("/signUp")}>
+          <Text >Sing Up</Text>
+        </Pressable>
+      </View>
+      
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'column',
+    backgroundColor: '#1869B2',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontFamily: 'Roboto',
+    color: 'white',
+    fontSize: 16,
+  },
+  imageContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: '5%',  /* 10% margin top */
+    marginBottom: '10%', /* 10% margin bottom */
+    marginLeft: 0,       /* No margin left */
+    marginRight: 0,      /* No margin right */
+    width: 289,
+    height: 223,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
+    marginLeft: 0,       /* No margin left */
+    marginRight: 0,    /* No margin right */
+  },
+  containerFields: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginLeft: '20%',       /* No margin left */
+    marginRight: '20%', 
+    marginBottom: '5%', /* 10% margin bottom */
+    
+  },
+  buttonContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'white',
+    textAlign: 'center',
+  },
+  buttons: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 80,
+    height: 30,
+    backgroundColor: '#3394EC',
+    borderRadius: 5,
+    padding: 5,
+    margin: 15,
+  },
+  inputFields: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    textAlign: 'center',
+    width: 250,
+    height: 35,
+    marginTop: 10,  /* 10% margin top */
+    marginBottom: 10, /* 10% margin bottom */
+    marginLeft: 0,       /* No margin left */
+    marginRight: 0,      /* No margin right */
+    borderRadius: 15,
+    padding: 10,
+  },
+
 });
+
+export default Login;
--- App/my-app/src/app/signUp.jsx
+++ App/my-app/src/app/signUp.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Text, TextInput, View } from 'react-native';
+import {Image, StyleSheet, Button, Pressable, Text, TextInput, View } from 'react-native';
 import { Link } from 'expo-router';
 import axios from 'axios';
 
@@ -47,31 +47,113 @@ function SignUp() {
   };
 
   return (
-    <View>
-      <Text>Sign Up</Text>
-      {errors.name && <Text style={{ color: 'red' }}>{errors.name}</Text>}
-      <TextInput
-        placeholder="Username"
-        value={name}
-        onChangeText={(text) => setName(text)}
-      />
-      {errors.email && <Text style={{ color: 'red' }}>{errors.email}</Text>}
-      <TextInput
-        placeholder="Email"
-        value={email}
-        onChangeText={(text) => setEmail(text)}
-      />
-      {errors.password && <Text style={{ color: 'red' }}>{errors.password}</Text>}
-      <TextInput
-        placeholder="Password"
-        value={password}
-        onChangeText={(text) => setPassword(text)}
-        secureTextEntry={true}
-      />
-      <Button title="Sign Up" onPress={handleSubmit} />
-      <Link href={"/login"}>Go Back</Link>
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={require('./img/Wolvesprojeto 1.png')} />
+      </View>
+        <Text style={styles.text}>Sign Up</Text>
+        <View style={styles.containerFields}>
+        {errors.name && <Text style={{ color: 'red' }}>{errors.name}</Text>}
+        <TextInput style={styles.inputFields}
+          placeholder="Username"
+          value={name}
+          onChangeText={(text) => setName(text)}
+        />
+        {errors.email && <Text style={{ color: 'red' }}>{errors.email}</Text>}
+        <TextInput style={styles.inputFields}
+          placeholder="Email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        {errors.password && <Text style={{ color: 'red' }}>{errors.password}</Text>}
+        <TextInput style={styles.inputFields}
+          placeholder="Password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          secureTextEntry={true}
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+      <Pressable style={styles.buttons} title="Sign Up" onPress={handleSubmit}>
+          <Text >Sing Up</Text>
+        </Pressable>
+      </View>
+      <Link style={styles.text} href={"/"}>Go Back</Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#1869B2',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontFamily: 'Roboto',
+    color: 'white',
+    fontSize: 16,
+  },
+  imageContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: '5%',  
+    marginBottom: '10%', 
+    marginLeft: 0,       
+    marginRight: 0,      
+    width: 289,
+    height: 223,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
+    marginLeft: 0,       
+    marginRight: 0,   
+  },
+  containerFields: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginLeft: '20%',       
+    marginRight: '20%', 
+    marginBottom: '5%', 
+  },
+  inputFields: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    textAlign: 'center',
+    width: 250,
+    height: 35,
+    marginTop: 10, 
+    marginBottom: 10, 
+    marginLeft: 0,      
+    marginRight: 0,      
+    borderRadius: 15,
+    padding: 10,
+  },
+  buttonContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'white',
+    textAlign: 'center',
+  },
+  buttons: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 80,
+    height: 30,
+    backgroundColor: '#3394EC',
+    borderRadius: 5,
+    padding: 5,
+    margin: 15,
+  },
+});
 
 export default SignUp;
